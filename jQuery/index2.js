0ac5aa1773dfb169d890c2851134248aaf0c61e7
gaeun9 <gaeun9809@naver.com>
2023-07-10 15:15:30 +0900

    제이쿼리 수업 종료

---
+++ jQuery/index2.js
@@ -0,0 +1,158 @@
+// jq val() 사용
+function getValue() {
+  // input의 value를 가지고와서
+  // alert 창에 띄워줌
+  const value = $("input").val();
+  alert(value);
+}
+function setValue() {
+  $("input").val("설정 완료 ~~");
+}
+
+/* css() 사용 */
+function changeCssJs() {
+  // span의 글씨 크기 20px
+  // 글자 색깔 빨간색
+  let span = document.querySelector("span");
+  span.style = "font-size: 20px; color: red;";
+}
+
+function changeCssJq() {
+  // 글자 크기 40px
+  // 글자 색깔 파란색
+  $("span").css("font-size", "40px");
+  $("span").css("color", "blue");
+}
+
+function getCssJq() {
+  // css 특정 속성값 가져오기
+  console.log($("span").css("color"));
+}
+
+/* attr() */
+function changeAttrJs() {
+  // 네이버로 이동
+  let a = document.querySelector("a");
+  a.setAttribute("href", "https://www.naver.com");
+  //   a.href = "https://www.naver.com";
+}
+function changeAttrJq() {
+  // 다음으로 이동
+  // target="_self"로 변경
+  $("a").attr("href", "https://www.daum.net");
+  $("a").attr("target", "_self");
+}
+
+/* text() */
+function changeTextJs() {
+  let p = document.querySelector(".p-text");
+  p.innerText = "js로 변경 &hearts;";
+}
+function changeTextJq() {
+  $(".p-text").text("jq로 변경 &hearts;");
+}
+
+/* html() */
+function changeHtmlJs() {
+  let p = document.querySelector(".p-html");
+  p.innerHTML = "js로 변경 &hearts;";
+}
+
+function changeHtmlJq() {
+  $(".p-html").html("jq로 변경 &hearts;");
+}
+
+/* 요소 추가하기 */
+function appendJs() {
+  let ul = document.querySelector(".colors");
+  let li = document.createElement("li");
+  li.innerText = "마지막 자식으로 추가된 js";
+
+  ul.append(li);
+}
+function appendJq() {
+  $(".colors").append("<li>마지막 자식으로 추가된 jq</li>");
+}
+
+function prependJs() {
+  let ul = document.querySelector(".color");
+  let li = document.createElement("li");
+  li.innerHTML = "<h4>첫 자식으로 추가된 js</h4>";
+
+  ul.prepend(li);
+}
+function prependJq() {
+  $(".color").prepend("<li><h4>첫 자식으로 추가된 jq</li>");
+  // jquery는 문자열에 태그 입력시 html 태그로 받아들임!
+}
+
+function beforeJs() {
+  let green = document.querySelector(".green");
+  let li = document.createElement("li");
+  li.innerText = "이전 형제요소로 추가된 js";
+
+  green.before(li);
+}
+function beforeJq() {
+  $(".green").before("<li>이전 형제 요소로 추가된 jq</li>");
+}
+
+function afterJs() {
+  let green = document.querySelector(".green");
+  let li = document.createElement("li");
+  li.innerText = "다음 형제 요소로 추가된 js";
+}
+function afterJq() {
+  $(".green").after("<li>다음 형제 요소로 추가된 jq</li>");
+}
+
+function removeJs() {
+  let li2 = document.querySelector("#li2");
+  li2.remove();
+}
+function removeJq() {
+  $("#li2").remove();
+}
+
+function emptyJq() {
+  let nums = document.querySelector("ul.nums");
+  nums.innerHTML = "";
+}
+
+function emptyJq() {
+  $("ul.nums").empty();
+}
+
+function findParent() {
+  // child2 부모 콘솔창에 출력
+  console.log($(".child2").parent());
+}
+function findParents() {
+  // child2 조상 콘솔창에 출력
+  console.log($(".child2").parents());
+}
+function findNext() {
+  // child2 다음 형제 요소 출력
+  console.log($(".child2").next());
+}
+function findPrev() {
+  // child2 이전 형제 요소 출력
+  console.log($(".child2").prev());
+}
+function findChildren() {
+  // parent 자식 요소 출력
+  console.log($(".parent").children());
+}
+
+function addClass() {
+  $("#hi").addClass("fs-50");
+}
+function removeClass() {
+  $("#hi").removeClass("fs-50");
+}
+function hasClass() {
+  console.log($("#hi").hasClass("fs-50"));
+}
+function toggleClass() {
+  $("#hi").toggleClass("bg-pink");
+}
