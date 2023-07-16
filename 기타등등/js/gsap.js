70e499e4a16a572a03624b6281d1f03eedc5a485
gaeun9 <gaeun9809@naver.com>
2023-07-17 01:01:07 +0900

    파일 경로 수정

--- 기타등등/js/gsap.js
+++ 기타등등/js/gsap.js
@@ -14,7 +14,6 @@ tl1
     },
     "<"
   )
-
   .to(".sun", {
     scale: 1.2,
     opacity: 0.5,
@@ -26,27 +25,28 @@ ScrollTrigger.create({
   animation: tl1,
   trigger: ".section1",
   start: "top top",
-  ent: "bottom, top",
+  end: "bottom top",
   scrub: true,
-  markers: true,
+
   pin: true,
+  end: "+=5000",
 });
 
 // animation 적용할 요소가 하나뿐이기 때문에
 // from 내부에 scrollTrigger 작성해도 됨
 let tl2 = gsap.timeline();
 tl2.from(".section2 div", {
-  ScrollTrigger: {
+  scrollTrigger: {
     animation: tl2,
     trigger: ".section2",
     start: "top top",
     end: "bottom 100%",
     end: "+=3000",
     scrub: 2,
-    pin: trun,
+    pin: true,
     markers: true,
   },
   rotate: 720,
-  opacitiy: 0,
+  opacity: 0,
   scale: 0,
 });
